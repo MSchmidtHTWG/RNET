33c7555bf944eaaeb9927f5349b48ff222bd58eb
MSchmidtHTWG <schmidtmaxhome@yahoo.de>
2022-05-30 18:12:20 +0200

    minor changes

--- P2P chat/codec.py
+++ P2P chat/codec.py
@@ -1,20 +1,20 @@
 import socket
 from struct import unpack, pack
 
-def registerRequestB(nick):
-    return pack(f'BB{len(nick)}s', 1, len(nick), nick.encode('utf-8'))
+def registerRequest_encode(nick, ip, port):
+    return pack(f'BB{len(nick)}sIH', 1, len(nick), nick.encode('utf-8'), socket.inet_aton(ip), port)
 
-def addUserB(nick, ip, port):
-    return pack(f'BB{len(nick)}sIH', 3, len(nick), nick.encode('utf-8'), ip, port)
+def addUser_encode(nick, ip, port):
+    return pack(f'BB{len(nick)}sIH', 3, len(nick), nick.encode('utf-8'), socket.inet_aton(ip), port)
 
-def broadcastMsgB(msg):
+def broadcastMsg_encode(msg):
     return pack(f'BB{len(msg)}s', 5, len(msg), msg.encode('utf-8'))
 
-def broadcastB(nick, msg):
+def broadcast_encode(nick, msg):
     return pack(f'BB{len(nick)}sB{len(msg)}s', 6, len(nick), nick.encode('utf-8'), len(msg), msg.encode('utf-8'))
 
-def logoutB():
+def logout_encode():
     return pack(f'B', 7)
 
-def removeUserB(nick):
+def removeUser_encode(nick):
     return pack(f'BB{len(nick)}s', 8, len(nick), nick.encode('utf-8'))
--- P2P chat/server.py
+++ P2P chat/server.py
@@ -1,2 +1,30 @@
+import pstats
 import socket
+import threading
 from struct import unpack, pack
+from _thread import *
+
+class Server:
+    def __init__(self):
+        self.port = 5000
+        self.ip = '127.0.0.1'
+        self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.serverSocket.bind((self.ip, self.port))
+        self.lock = threading.Lock()
+        self.users = dict()
+        self.listen()
+    
+    def listen(self):
+        while True:
+            self.serverSocket.listen(2)
+            connectionSocket, addr = self.serverSocket.accept()
+            #thread mit socket und addr erstellen
+            self.lock.acquire()
+            start_new_thread(self.user, (connectionSocket, addr))
+            self.lock.release()
+            
+    def user(self, connectionSocket, addr):
+        # rcv reg req
+        # update dict
+        # wait for new msg
+        pass
